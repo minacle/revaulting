af8f2ecbbe80d07a4c4369a6c94e969ede249b2b
Mina Her <minacle@live.com>
2024-06-27 17:23:42 +0900

    Initiate

---
+++ README.md
@@ -0,0 +1,26 @@
+# Revaulting
+
+Revaulting is a plugin that improves the Vault system in the Minecraft 1.21 update by allowing players to reopen specific Vaults multiple times. With the appropriate Trial Key or Ominous Key, players can reopen Vaults they've already accessed, adding flexibility to the gameplay.
+
+## Key Features
+- Allows Vaults and Ominous Vaults to be reopened multiple times.
+- Uses Persistent Data to track the number of times each player opens Vaults.
+- Checks player location every 20 ticks to determine if they are within the Vault's detection range.
+- Updates Vault reward data so players can receive rewards again.
+
+## Installation
+1. Download the [Revaulting plugin](#).
+2. Place the plugin file into your server's `plugins` folder.
+3. Download and install [NBT-API](https://modrinth.com/plugin/nbtapi) plugin, if not already installed.
+4. Restart the server.
+
+## Usage
+1. Join the Minecraft server and start playing.
+2. Use the Trial Key or Ominous Key to reopen Vaults multiple times.
+3. Every 20 ticks, the plugin will check if players are within the detection range of the Vault, allowing them to receive rewards.
+
+## Contribution and Support
+Revaulting is an open-source project, and contributions are welcome. If you encounter any issues or have suggestions for improvements, please report them on the GitHub issue tracker.
+
+## License
+Revaulting is licensed under [The Unlicense](https://unlicense.org/).
---
+++ src/main/java/moe/minacle/minecraft/plugins/revaulting/Plugin.java
@@ -0,0 +1,187 @@
+package moe.minacle.minecraft.plugins.revaulting;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.Vault;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import de.tr7zw.nbtapi.NBT;
+import de.tr7zw.nbtapi.NBTType;
+import de.tr7zw.nbtapi.handler.NBTHandlers;
+import de.tr7zw.nbtapi.iface.ReadWriteNBT;
+import de.tr7zw.nbtapi.iface.ReadWriteNBTList;
+import de.tr7zw.nbtapi.utils.Metrics;
+import io.papermc.paper.math.BlockPosition;
+import io.papermc.paper.math.Position;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
+
+public class Plugin extends JavaPlugin implements Listener {
+
+    private static final int BSTATS_PLUGIN_ID = 22418;
+
+    private static final @NotNull UUID UUID_ZERO = new UUID(0, 0);
+
+    private final @NotNull String REWARDED_PLAYERS = Objects.requireNonNull(namespacedKey("rewarded_players")).toString();
+
+    private final @NotNull String REWARDED_COUNTS = Objects.requireNonNull(namespacedKey("rewarded_counts")).toString();
+
+    private @Nullable ScheduledTask vaultManipulationTask;
+
+    private @Nullable NamespacedKey namespacedKey(final @NotNull String key) {
+        return NamespacedKey.fromString(key, this);
+    }
+
+    private @NotNull Set<BlockPosition> collectBlockPositions(final @NotNull World world) {
+        List<Player> players = world.getPlayers();
+        HashSet<BlockPosition> blockPositions = HashSet.newHashSet(343 * players.size());
+        Location location;
+        int blockX;
+        int blockY;
+        int blockZ;
+        int x;
+        int y;
+        int z;
+        for (final Player player : players) {
+            if (!player.isConnected() || player.getGameMode() == GameMode.SPECTATOR)
+                continue;
+            location = player.getLocation();
+            if (location == null)
+                continue;
+            blockX = location.getBlockX();
+            blockY = location.getBlockY();
+            blockZ = location.getBlockZ();
+            for (x = blockX - 3; x < blockX + 3; x++)
+                for (y = blockY - 3; y < blockY + 3; y++)
+                    for (z = blockZ - 3; z < blockZ + 3; z++)
+                        blockPositions.add(Position.block(x, y, z));
+        }
+        return blockPositions;
+    }
+
+    private @NotNull Set<Block> collectVaults(final @NotNull World world, final @NotNull Set<BlockPosition> blockPositions) {
+        HashSet<Block> vaults = new HashSet<>();
+        Block block;
+        for (final BlockPosition blockPosition : blockPositions) {
+            block = world.getBlockAt(blockPosition.blockX(), blockPosition.blockY(), blockPosition.blockZ());
+            if (block.getType() == Material.VAULT)
+                vaults.add(block);
+        }
+        return vaults;
+    }
+
+    private void manipulateVault(final @NotNull Block block) {
+        final Vault vault;
+        final List<UUID> newRewardedPlayers;
+        if (block.getType() != Material.VAULT)
+            return;
+        vault = (Vault)block.getState();
+        newRewardedPlayers =
+            NBT.modify(
+                vault,
+                (nbt) -> {
+                    ReadWriteNBT serverData = nbt.getCompound("server_data");
+                    ReadWriteNBTList<UUID> rewardedPlayers;
+                    List<UUID> result;
+                    if (serverData == null)
+                        return null;
+                    rewardedPlayers = serverData.getUUIDList("rewarded_players");
+                    if (rewardedPlayers == null)
+                        return null;
+                    result = rewardedPlayers.toListCopy();
+                    rewardedPlayers.clear();
+                    return result;
+                });
+        if (newRewardedPlayers == null || newRewardedPlayers.isEmpty())
+            return;
+        NBT.modifyPersistentData(
+            vault,
+            (nbt) -> {
+                ReadWriteNBTList<UUID> rewardedPlayers = nbt.getUUIDList(REWARDED_PLAYERS);
+                ReadWriteNBTList<Integer> rewardedCounts = nbt.getIntegerList(REWARDED_COUNTS);
+                int rewardedPlayersSize;
+                int rewardedCountsSize;
+                int index;
+                int count;
+                if (rewardedPlayers == null) {
+                    ReadWriteNBT uuidList = NBT.parseNBT("[[I;0,0,0,0]]");
+                    nbt.set(REWARDED_PLAYERS, uuidList, NBTHandlers.STORE_READWRITE_TAG);
+                    rewardedPlayers = nbt.getUUIDList(REWARDED_PLAYERS);
+                }
+                if (rewardedCounts == null) {
+                    ReadWriteNBT integerList = NBT.parseNBT("[0]");
+                    nbt.set(REWARDED_COUNTS, integerList, NBTHandlers.STORE_READWRITE_TAG);
+                    rewardedCounts = nbt.getIntegerList(REWARDED_COUNTS);
+                }
+                rewardedPlayersSize = rewardedPlayers.size();
+                rewardedCountsSize = rewardedCounts.size();
+                if (rewardedPlayersSize > rewardedCountsSize)
+                    for (index = rewardedCountsSize; index < rewardedPlayersSize; index++)
+                        rewardedCounts.add(1);
+                else if (rewardedPlayersSize < rewardedCountsSize)
+                    for (index = rewardedPlayersSize; index < rewardedCountsSize; index++)
+                        rewardedCounts.remove(rewardedPlayersSize);
+                for (final UUID player : newRewardedPlayers) {
+                    index = rewardedPlayers.indexOf(player);
+                    if (index == -1) {
+                        rewardedPlayers.add(player);
+                        rewardedCounts.add(1);
+                    }
+                    else {
+                        count = rewardedCounts.get(index);
+                        rewardedCounts.set(index, count + 1);
+                    }
+                }
+                if (rewardedPlayers.get(0).equals(Plugin.UUID_ZERO)) {
+                    rewardedPlayers.remove(0);
+                    rewardedCounts.remove(0);
+                }
+                if (nbt.hasTag("__nbtapi", NBTType.NBTTagString))
+                    nbt.removeKey("__nbtapi");
+            });
+    }
+
+    // MARK: JavaPlugin
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        Server server = getServer();
+        new Metrics(this, BSTATS_PLUGIN_ID);
+        server.getPluginManager().registerEvents(this, this);
+        vaultManipulationTask =
+            server.getGlobalRegionScheduler().runAtFixedRate(
+                this,
+                ($0) -> {
+                    for (final World world : server.getWorlds())
+                        for (final Block block : collectVaults(world, collectBlockPositions(world)))
+                            manipulateVault(block);
+                },
+                20,
+                20);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        if (vaultManipulationTask != null) {
+            if (!vaultManipulationTask.isCancelled())
+                vaultManipulationTask.cancel();
+            vaultManipulationTask = null;
+        }
+    }
+}
